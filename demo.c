eba21473a4fa3db72d7f49e6de4f78b994476d45
Felix Houghton <fdphoughton@protonmail.com>
2025-10-18 23:28:20 +0100

    Add basic input handling to demo/SDL2 backend

--- demo.c
+++ demo.c
@@ -3,17 +3,28 @@
 
 int main() {
 	XGA_init();
-	XGA_color RED = {255, 0, 0};
+	const XGA_color RED = {255, 0, 0};
+	const XGA_color GREEN = {0, 255, 0};
+	const XGA_color BLUE = {0, 0, 255};
+	XGA_color line_color = RED;
 
 	while (true) {
+		// Update
 		XGA_update();
+		if (XGA_isAPressed()) {
+			line_color = GREEN;
+		}
+		if (XGA_isBPressed()) {
+			line_color = BLUE;
+		}
+
+		// Draw
 		int i = 0; 
 		for (i = 0; i < SCREEN_WIDTH; i++) {
-			XGA_drawPixel(i, i, RED); 
+			XGA_drawPixel(i, i, line_color); 
 		}
 		XGA_drawScreen();
 		XGA_wait(1/60);
-		RED.g += 1;
 	}
 	XGA_exit();
 	return 0;
--- src/xga_sdl2.c
+++ src/xga_sdl2.c
@@ -54,6 +54,14 @@ void XGA_drawScreen() {
 	SDL_RenderPresent(renderer);
 }
 
+
+bool XGA_key_UP = false;
+bool XGA_key_LEFT = false;
+bool XGA_key_DOWN = false;
+bool XGA_key_RIGHT = false;
+bool XGA_key_A = false;
+bool XGA_key_B = false;
+
 void XGA_update() {
 	SDL_Event e;
 
@@ -64,10 +72,45 @@ void XGA_update() {
 		{
 			XGA_exit();
 		}
+		// Map inputs to global to for other functions
+		else if( e.type == SDL_KEYDOWN )
+                {
+			switch (e.key.keysym.sym)
+			{
+				case SDLK_w: XGA_key_UP = true; break;
+				case SDLK_a: XGA_key_LEFT = true; break;
+				case SDLK_s: XGA_key_DOWN = true; break;
+				case SDLK_d: XGA_key_RIGHT = true; break;
+				case SDLK_m: XGA_key_A = true; break;
+				case SDLK_n: XGA_key_B = true; break;
+				default: break;
+			}
+		}
+		else if (e.type == SDL_KEYUP)
+		{
+			switch (e.key.keysym.sym)
+			{
+				case SDLK_w: XGA_key_UP = false; break;
+				case SDLK_a: XGA_key_LEFT = false; break;
+				case SDLK_s: XGA_key_DOWN = false; break;
+				case SDLK_d: XGA_key_RIGHT = false; break;
+				case SDLK_m: XGA_key_A = false; break;
+				case SDLK_n: XGA_key_B = false; break;
+				default: break;
+			}
+		}
 	}
 }
 
 void XGA_wait(float seconds) {
 	float ms = seconds * 1000;
 	SDL_Delay((int)ms);
+}
+
+int XGA_isAPressed() {
+	return XGA_key_A;
+}
+
+int XGA_isBPressed() {
+	return XGA_key_B;
 }
